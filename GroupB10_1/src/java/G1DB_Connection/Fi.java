616f836bd468b33e98fe8e0a075d3f714d444fcf
Renad-Aljehani <Renadsalehjh@gmail.com>
2023-01-29 22:19:45 +0300

    Renad commit

---
+++ GroupB10_1/src/java/G1DB_Connection/Fi.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package G1DB_Connection;
+
+/**
+ *
+ * @author Renad Aljehani
+ */
+public class Fi {
+    
+}
